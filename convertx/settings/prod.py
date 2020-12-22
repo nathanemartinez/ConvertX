a3095ccfb6e4c3c8d2147aa01991aa10c6ec799d
Nathan Martinez <nathanemtz@gmail.com>
2020-12-21 22:04:31 -0800

    static files

--- convertx/settings/prod.py
+++ convertx/settings/prod.py
@@ -23,5 +23,5 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 django_heroku.settings(locals())
