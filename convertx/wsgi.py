3ce04bbeb5790c4a1e6c6140030552f73945f4b3
Nathan Martinez <nathanemtz@gmail.com>
2020-12-21 22:31:54 -0800

    wsgi fixed settings

--- convertx/wsgi.py
+++ convertx/wsgi.py
@@ -5,5 +5,5 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'convertx.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.environ.get('convertx_settings'))
 application = get_wsgi_application()
