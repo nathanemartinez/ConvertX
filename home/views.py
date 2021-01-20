77c0ec51539bc285ad275f9b7ab84767a6d4e9b4
Nathan <nathanemtz@gmail.com>
2021-01-19 20:46:10 -0800

    integrated sentry with error pages

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from sentry_sdk import capture_message
 
 
 class HomeView(TemplateView):
@@ -7,17 +8,21 @@ class HomeView(TemplateView):
 
 
 def error_400(request, exception=None):
+    capture_message("400 error - custom page", level="error")
     return render(request, 'home/error_pages/400.html')
 
 
 def error_403(request, exception=None):
+    capture_message("403 error - custom page", level="error")
     return render(request, 'home/error_pages/403.html')
 
 
 def error_404(request, exception=None):
+    capture_message("404 error - custom page", level="error")
     return render(request, 'home/error_pages/404.html')
 
 
 def error_500(request, exception=None):
+    capture_message("500 error - custom page", level="error")
     return render(request, 'home/error_pages/500.html')
 
