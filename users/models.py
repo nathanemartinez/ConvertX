1fefb42d03210be334ea78c055ecad56e8b4bf97
Nathan <nathanemtz@gmail.com>
2021-01-29 21:34:58 -0800

    comments

--- users/models.py
+++ users/models.py
@@ -1,7 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group
 from simple_history.models import HistoricalRecords
 
 
 class User(AbstractUser):
     history = HistoricalRecords()
+
+
+# Group.add_to_class()
+# Group.add_to_class('description', models.CharField(max_length=180,null=True, blank=True))
