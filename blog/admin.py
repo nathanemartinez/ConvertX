ede73be58b73088878f333cfa25ab6e5b09c0f19
Nathan <nathanemtz@gmail.com>
2021-01-30 01:49:15 -0800

    testing guardian admin

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,8 @@ from .models import (AffiliateImage, AffiliateLink, AffiliateProduct, AffiliateP
 					 InfoPostEstimates, NormalImage, NormalLink, ReviewPost, ReviewPostEstimates, Tag, TopMoneyPost,
 					 TopMoneyPostEstimates,)
 from simple_history.admin import SimpleHistoryAdmin
+from guardian.admin import GuardedModelAdmin
+
 # from .models import (AffiliateImage, AffiliateLink, AffiliateProduct, AffiliateProgram, Category, ImageMixin, InfoPost,
 # 					 InfoPostEstimates, LinkMixin, NameMixin, NormalImage, NormalLink, PostEstimatesMixin, PostMixin,
 # 					 ProductMixin, ReviewPost, ReviewPostEstimates, Tag, TimeStampCreatorMixin, TopMoneyPost,
@@ -15,8 +17,15 @@ from simple_history.admin import SimpleHistoryAdmin
 #
 # admin.site.register(models_with_mixins)
 
-models_no_mixins = [AffiliateImage, AffiliateLink, AffiliateProduct, AffiliateProgram, Category, InfoPost,
+models_no_mixins = [AffiliateLink, AffiliateProduct, AffiliateProgram, Category, InfoPost,
 					InfoPostEstimates, NormalImage, NormalLink, ReviewPost, ReviewPostEstimates, Tag, TopMoneyPost,
 					TopMoneyPostEstimates,]
 
 admin.site.register(models_no_mixins, SimpleHistoryAdmin)
+
+
+class AffiliateImageAdmin(GuardedModelAdmin):
+	pass
+
+
+admin.site.register(AffiliateImage, AffiliateImageAdmin)
