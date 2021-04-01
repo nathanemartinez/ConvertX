83bd45ab944461574fc3c7e9f5f5612d1721187b
Nathan <nathanemtz@gmail.com>
2021-04-01 14:36:47 -0700

    created association model to replace group model

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, Group
+from django.contrib.auth.models import AbstractUser, Permission
 from simple_history.models import HistoricalRecords
+from users.managers import AssociationManager
+from django.utils.translation import gettext as _
 
 
 class User(AbstractUser):
@@ -12,5 +14,19 @@ class User(AbstractUser):
             return True
         return False
 
-# Group.add_to_class('description', models.TextField(max_length=150, null=True, blank=False))
 
+class Association(models.Model):
+    name = models.CharField(_('name'), max_length=150, unique=True)
+    permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=_('Permissions'),
+        blank=True,
+    )
+    objects = AssociationManager()
+
+    class Meta:
+        verbose_name = _('Association')
+        verbose_name_plural = _('Associations')
+
+    def __str__(self):
+        return self.name
