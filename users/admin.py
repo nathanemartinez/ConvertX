28f7315ae6c5381b96c813d829588445666f4fa6
Nathan <nathanemtz@gmail.com>
2021-04-16 00:28:25 -0700

    re registering 'Sites' app

--- users/admin.py
+++ users/admin.py
@@ -2,10 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group, Permission
 from users.models import User, Association
-from django.contrib.sites.models import Site
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Permission)
 admin.site.register(Association)
 admin.site.unregister(Group)
-admin.site.unregister(Site)
