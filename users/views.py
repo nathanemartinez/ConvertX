91deaf1db66e509f264e802ca631fb0342e2bbee
Nathan <nathanemtz@gmail.com>
2021-04-17 16:09:48 -0700

    saving

--- users/views.py
+++ users/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import UpdateView
+from django.contrib.auth import get_user_model
+from users.forms import UserModelForm
+
+User = get_user_model()
+
+
+# class CategoryUpdateView(UpdateView):
+# 	model = User
+# 	form_class = UserModelForm
+	# template_name =
 
 
 def lockout(request):
 	return render(request, 'users/general/lockout.html')
+
