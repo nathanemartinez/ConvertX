882bcbd7e3553c04bb7a03e0884db5209338e830
Nathan <nathanemtz@gmail.com>
2021-04-17 21:16:09 -0700

    fixed: update user - superuser checks

--- users/views.py
+++ users/views.py
@@ -1,15 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from django.views.generic import UpdateView
 from django.contrib.auth import get_user_model
 from users.forms import UserModelForm
+from django.contrib.auth.mixins import UserPassesTestMixin
 
 User = get_user_model()
 
 
-# class UserUpdateView(UpdateView):
-# 	model = User
-# 	form_class = UserModelForm
-# 	template_name =
+class UserUpdateView(UserPassesTestMixin, UpdateView):
+	model = User
+	form_class = UserModelForm
+	template_name = 'users/user/update.html'
+
+	def get_success_url(self):
+		return reverse('home:home')
+
+	def test_func(self):
+		is_superuser = self.request.user.is_superuser
+		is_user = self.request.user == self.get_object()
+		return is_superuser and is_user
 
 
 def lockout(request):
