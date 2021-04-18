5359c9b542e72f716f43f4d45440ed2fbdc8f036
Nathan <nathanemtz@gmail.com>
2021-04-18 01:28:06 -0700

    added: domain names to project constants

--- convertx/constants.py
+++ convertx/constants.py
@@ -5,6 +5,9 @@ import os
 DOTENV_PATH = r'C:\Users\natha\PycharmProjects\convertx\env\.env'  # this is a constant
 load_dotenv(dotenv_path=DOTENV_PATH)
 
+DOMAIN_NAME = 'convertxproject.com'
+DOMAIN_NAME_WWW = 'www.convertxproject.com'
+DOMAIN_NAME_HTTPS = 'https://convertxproject.com'
 
 SUPERUSER_USERNAME = os.getenv('SUPERUSER_USERNAME')
 SUPERUSER_EMAIL = os.getenv('SUPERUSER_EMAIL')
