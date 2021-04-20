0f19836ca684c7ad996fbbab3afc8ba26944efaf
Nathan <nathanemtz@gmail.com>
2021-04-20 16:23:28 -0700

    changed: ww redirect middleware - print statements and variable name changes

--- convertx/middleware.py
+++ convertx/middleware.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponsePermanentRedirect
-from convertx.constants import DOMAIN_NAME_WWW, DOMAIN_NAME_HTTPS
+from convertx.constants import DOMAIN_NAME_WWW, DOMAIN_NAME
 
 
 class WWWRedirectMiddleware:
@@ -9,8 +9,11 @@ class WWWRedirectMiddleware:
     def __call__(self, request):
         host = request.get_host().partition(':')[0]
         if host == DOMAIN_NAME_WWW:
+            print(host)
+            print(DOMAIN_NAME_WWW)
+            print(DOMAIN_NAME + request.path)
             return HttpResponsePermanentRedirect(
-                DOMAIN_NAME_HTTPS + request.path
+                DOMAIN_NAME + request.path
             )
         else:
             return self.get_response(request)
