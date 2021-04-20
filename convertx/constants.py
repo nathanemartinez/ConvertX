d2555f4122fb8e361589bb589d88ba76329ec308
Nathan <nathanemtz@gmail.com>
2021-04-20 16:23:03 -0700

    removed: 'DOMAIN_NAME_HTTPS'

--- convertx/constants.py
+++ convertx/constants.py
@@ -7,7 +7,6 @@ load_dotenv(dotenv_path=DOTENV_PATH)
 
 DOMAIN_NAME = 'convertxproject.com'
 DOMAIN_NAME_WWW = 'www.convertxproject.com'
-DOMAIN_NAME_HTTPS = 'https://convertxproject.com'
 
 SUPERUSER_USERNAME = os.getenv('SUPERUSER_USERNAME')
 SUPERUSER_EMAIL = os.getenv('SUPERUSER_EMAIL')
