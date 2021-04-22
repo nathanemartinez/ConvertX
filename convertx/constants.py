b094aa6740db2538025d24177db999e5bbffc6b0
Nathan <nathanemtz@gmail.com>
2021-04-22 01:05:59 -0700

    added: heroku domain

--- convertx/constants.py
+++ convertx/constants.py
@@ -8,6 +8,7 @@ load_dotenv(dotenv_path=DOTENV_PATH)
 DOMAIN_NAME = 'convertxproject.com'
 DOMAIN_NAME_WWW = 'www.convertxproject.com'
 DOMAIN_NAME_HTTPS = 'https://convertxproject.com'
+DOMAIN_HEROKU = 'convertxproject.herokuapp.com'
 
 SUPERUSER_USERNAME = os.getenv('SUPERUSER_USERNAME')
 SUPERUSER_EMAIL = os.getenv('SUPERUSER_EMAIL')
